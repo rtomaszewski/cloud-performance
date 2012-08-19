044da284db0d6758fa4d1e5e957294c913b6f8e3
Radoslaw Tomaszewski <radoslaw.tomaszewski@gmail.com>
2012-08-19 18:53:17 +0100

    fixed logf

--- myaliases.sh
+++ myaliases.sh
@@ -1,5 +1,7 @@
 alias logclean='for i in $(ls log.*.txt) ; do echo $i; mv $i{,.done};  done'
-alias logf='ls -1tr *.txt | tail -1 | xargs cat '
+alias logf='ls -1tr log*.txt | tail -1 | xargs cat '
 alias loguser='logf  | egrep -i "^\[|^test" | egrep -v "Server: csperform"'
 alias logdebug='logf  | egrep -i debug'
+
+alias logkill='kill $(ps | grep python| awk "{ print \$1}"); fg'
 
